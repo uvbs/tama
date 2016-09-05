f7c83283ce01ba354fc01f6b03432a642c82e4ab
xahgo <xuzhu74@gmail.com>
2016-09-05 09:35:06 +0900

    1

---
+++ Caribe/App/game_src/XSplitNode.cpp
@@ -0,0 +1,48 @@
+﻿#include "stdafx.h"
+#include "XSplitNode.h"
+
+#ifdef WIN32
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+#endif
+
+XE_NAMESPACE_START( xSplit )
+
+// id를 가진 이미지(크기를 가진)를 트리에 삽입한다.
+XNode* XNode::Insert( ID idImg, const XE::VEC2& sizeImg )
+{
+	if( IsLeaf() ) {
+
+	} else {
+		if( m_idImg ) {
+			return nullptr;
+		}
+		const auto wImg = sizeImg.w;
+		const auto hImg = sizeImg.h;
+		const auto wRect = m_Rect.GetWidth();
+		const auto hRect = m_Rect.GetHeight();
+		// 삽입할 이미지가 노드크기보다 크면 널리턴
+		if( sizeImg.w >= wRect && sizeImg.h >= hRect ) {
+			return nullptr;
+		}
+		// ??
+		if( IsFitPerfectly( sizeImg ) ) {
+			return this;
+		}
+		m_Child[0] = new XNode();
+		m_Child[1] = new XNode();
+		const XE::VEC2 vd( wRect - sizeImg.w, hRect - sizeImg.h );
+		if( vd.w > vd.h ) {
+			const XE::VEC2 sizeSpace = XE::VEC2( wImg, m_Rect.GetHeight() );
+		} else {
+			const XE::VEC2 sizeSpace = XE::VEC2( wRect, hImg );
+			m_Child[0]->SetRect( XE::xRECT( m_Rect.vLT, sizeSpace ) );
+			m_Child[1]->SetRect( XE::xRECT()
+		}
+	}
+}
+
+XE_NAMESPACE_END;	// xSplit
---
+++ Caribe/App/game_src/XSplitNode.h
@@ -0,0 +1,38 @@
+﻿/********************************************************************
+	@date:	동적 텍스쳐 아틀라스를 위한 공간 분할
+	@file: 	
+	@author:	xuzhu
+	
+	@brief:	
+*********************************************************************/
+#pragma once
+
+#include "etc/Global.h"
+#include "XE.h"
+
+XE_NAMESPACE_START( xSplit )
+class XNode;
+// 벽(?)과 공간을 나타내는 노드
+class XNode {
+public:
+	XNode() {}
+	~XNode() {}
+	//
+	GET_SET_ACCESSOR_CONST( const XE::xRECT&, Rect );
+	//
+	XNode* Insert( ID idImg, const XE::VEC2& sizeImg );
+	inline bool IsLeaf() const {
+		return aaa;
+	}
+	// ??
+	inline bool IsFitPerfectly( const XE::VEC2& sizeImg ) const {
+		return bbb;
+	}
+private:
+	XNode* m_Child[2] = {nullptr, nullptr};
+	XE::xRECT m_Rect;			// 분할전 전체 공간의 위치와 크기
+	ID m_idImg = 0;
+};
+
+
+XE_NAMESPACE_END;
