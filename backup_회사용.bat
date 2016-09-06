e997712bbd90fe76fce17d06ab4bfacf82f9e435
xahgo <xuzhu74@gmail.com>
2016-09-06 15:47:30 +0900

    test

--- backup_회사용.bat
+++ backup_회사용.bat
@@ -1,3 +1,4 @@
 rem xcopy c:\xuzhu_work\Project\iPhone_zero d:\_backup\today /a /e /y /exclude:exclude.txt /l  >> dir_copy.txt
 xcopy c:\xuzhu_work\Project\iPhone_zero d:\_backup\today /a /e /y /exclude:exclude.txt
 pause
+
